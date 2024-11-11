0e3faaff897cb86a8679cd8f30e391f9c0100f46
sablenagaraj2070 <sablenagaraj123@gmail.com>
2024-11-11 13:42:41 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,21 +1,28 @@
 class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-        r=len(grid)
-        c=len(grid[0])
-        seen=set()
-        ans=0
-        def help(i,j):
-            if i<0 or i>=r or j<0 or j>=c or grid[i][j]=='W' or (i,j) in seen:
-                return
-            seen.add((i,j))
-            help(i+1,j)
-            help(i,j+1)
-            help(i-1,j)
-            help(i,j-1)
+        r, c = len(grid), len(grid[0])
+        seen = set()
+        ans = 0
+
+        def help(i, j):
+            # Stack-based DFS to avoid recursion depth limits for large grids
+            stack = [(i, j)]
+            while stack:
+                x, y = stack.pop()
+                if (x, y) in seen or grid[x][y] == 'W':
+                    continue
+                seen.add((x, y))
+                
+                # Check all four directions (up, down, left, right)
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < r and 0 <= ny < c and grid[nx][ny] == 'L':
+                        stack.append((nx, ny))
+
         for i in range(r):
             for j in range(c):
-                if (i,j) not in seen and grid[i][j]=='L':
-                    help(i,j)
-                    ans+=1
+                if grid[i][j] == 'L' and (i, j) not in seen:
+                    help(i, j)  # Start DFS from this cell
+                    ans += 1   # Found a new island, increment count
                     
         return ans
