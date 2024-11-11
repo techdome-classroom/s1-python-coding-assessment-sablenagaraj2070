cb7180d13e65180117e9ba963235be465492f919
sablenagaraj2070 <sablenagaraj123@gmail.com>
2024-11-11 13:33:28 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,14 +1,5 @@
 class Solution:
-   
-    # def getTotalIsles(self, grid: list[list[str]]) -> int:
-    # #    write your code here
-      
-                    
-    #     return 0
-    # class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
         r=len(grid)
         c=len(grid[0])
         seen=set()
